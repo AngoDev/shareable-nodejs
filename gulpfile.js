39702d5e9f4f4107b6583f6d8bbd40d0c6df68a4
Flowck II <flowck96@hotmail.com>
2015-08-29 23:42:05 +0100

    Add Gulp

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// Require some modules
+var gulp = require("gulp");
+var stylus = require("gulp-stylus");
+var watch = require("gulp-watch");
+
+// Gulp tasks
+gulp.task("stylus", function(){
+
+	watch("./app/public/css/stylus/*.styl", function(){
+		// Compile stylus to css
+		gulp.src("./app/public/css/stylus/*.styl")
+			.pipe(stylus({compress: true}))
+			.pipe(gulp.dest("./app/public/css/"));
+	})
+	
+});
