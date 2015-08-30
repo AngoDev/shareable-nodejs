ac67344bd26cb2db541e75f23e1b3218910aaabd
Flowck II <flowck96@hotmail.com>
2015-08-30 08:25:48 +0100

    Config inert to support static files

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 // Require modules
 var Hapi = require("hapi");
 var routes = require("./app/routes/routes");
+var inert = require("inert");
+var path = require("path");
 var configs = require("./app/configs");
 
 // Init the server
-var app = new Hapi.Server();
-app.connection({port: 3000});
+var app = new Hapi.Server({
+	connections: {
+		routes: {
+			files: {
+				relativeTo: path.join(__dirname, '/app/public')
+			}
+		}
+	}
+});
+
+app.connection({
+	port: 3000
+});
 
 // Init configs
 configs(app);
