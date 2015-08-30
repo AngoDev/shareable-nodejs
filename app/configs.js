4491db181a943d151b155daee863d4fb49253820
Flowck II <flowck96@hotmail.com>
2015-08-30 08:25:26 +0100

    Support static files

--- app/configs.js
+++ app/configs.js
@@ -1,10 +1,15 @@
 // Require modules
 var jade = require("jade");
 var vision = require("vision");
+var inert = require("inert");
 
 module.exports = function(app){
+
 	
 	app.register(vision, function(err){
+
+		// Handle with the error
+		if(err) throw err;
 
 		app.views({
 			engines: {
@@ -16,4 +21,17 @@ module.exports = function(app){
 
 	});
 
+	app.register(inert, function(){});
+
+	app.route({
+		method: 'GET',
+		path: "/{params*}",
+		handler: {
+			directory: {
+				path: "."
+			}
+		}
+	});
+
 }
+
